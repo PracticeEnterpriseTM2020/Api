0f128358c278421cb8980d9aa636b5da887ec42f
axel <r0746357@student.thomasmore.be>
2020-02-19 16:31:57 +0100

    migrations added

---
+++ EnerjoyApi/database/migrations/2020_02_19_101140_create_customers_table.php
@@ -0,0 +1,35 @@
+<?php
+namespace api;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->bigIncrements('id')->unsigned();
+            $table->primary('id');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('email')->unique();
+            $table->string('password');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
---
+++ EnerjoyApi/database/migrations/2020_02_19_101756_create_addresses_table.php
@@ -0,0 +1,36 @@
+<?php
+namespace api;
+use App\customer;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string("address");
+            $table->string("number");
+            $table->string("city");
+            $table->string("postalcode");
+            $table->unsignedBigInteger("custId");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
