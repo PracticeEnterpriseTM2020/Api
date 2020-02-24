7b7406bbbc1108051225c1fd91a7deef326aa842
wocowoco <wouter360@hotmail.com>
2020-02-24 19:57:14 +0100

    [Database] Added dummy data in each table
    Added dummy data in each table for testing purposes (tablerelations/restrictions).
    Changed the position of the country column in the address table to the city table instead, since cities are linked to countries themselves.

--- Database/enerjoy.sql
+++ Database/enerjoy.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 24, 2020 at 05:29 PM
+-- Generation Time: Feb 24, 2020 at 07:54 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.2
 
@@ -32,9 +32,19 @@ CREATE TABLE `addresses` (
   `id` bigint(8) UNSIGNED NOT NULL,
   `street` varchar(70) NOT NULL,
   `number` varchar(7) NOT NULL,
-  `cityId` bigint(8) UNSIGNED NOT NULL,
-  `countryId` bigint(8) UNSIGNED NOT NULL
+  `cityId` bigint(8) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `addresses`
+--
+
+INSERT INTO `addresses` (`id`, `street`, `number`, `cityId`) VALUES
+(1, 'Teststraat', '100', 1),
+(2, 'Dummylaan', '37', 2),
+(3, 'Ruslaan', '5', 4),
+(4, 'Russtreet', '1337', 3),
+(5, 'Werknemerstraat', '1', 2);
 
 -- --------------------------------------------------------
 
@@ -44,9 +54,20 @@ CREATE TABLE `addresses` (
 
 CREATE TABLE `city` (
   `id` bigint(8) UNSIGNED NOT NULL,
+  `countryId` bigint(8) UNSIGNED NOT NULL,
   `name` varchar(30) NOT NULL,
-  `postalcode` varchar(6) NOT NULL
+  `postalcode` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `city`
+--
+
+INSERT INTO `city` (`id`, `countryId`, `name`, `postalcode`) VALUES
+(1, 1, 'Hulshout', '2235'),
+(2, 1, 'Mechelen', '2800'),
+(3, 2, 'Moscow', '101000'),
+(4, 2, 'Kazan', '420001');
 
 -- --------------------------------------------------------
 
@@ -58,6 +79,14 @@ CREATE TABLE `country` (
   `id` bigint(8) UNSIGNED NOT NULL,
   `name` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `country`
+--
+
+INSERT INTO `country` (`id`, `name`) VALUES
+(1, 'Belgium'),
+(2, 'Russia');
 
 -- --------------------------------------------------------
 
@@ -74,6 +103,14 @@ CREATE TABLE `customers` (
   `addressId` bigint(8) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`id`, `firstname`, `lastname`, `email`, `password`, `addressId`) VALUES
+(1, 'Peter', 'Selie', 'peter@selie.be', 'notencrypted', 1),
+(2, 'Dum', 'My', 'dummy@email.com', 'notasafepassword', 2);
+
 -- --------------------------------------------------------
 
 --
@@ -83,10 +120,20 @@ CREATE TABLE `customers` (
 CREATE TABLE `employees` (
   `id` bigint(8) UNSIGNED NOT NULL,
   `addressId` bigint(8) UNSIGNED NOT NULL,
+  `firstname` varchar(30) NOT NULL,
+  `lastname` varchar(30) NOT NULL,
   `email` varchar(255) NOT NULL,
   `phonenumber` varchar(20) NOT NULL,
   `jobId` bigint(8) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `employees`
+--
+
+INSERT INTO `employees` (`id`, `addressId`, `firstname`, `lastname`, `email`, `phonenumber`, `jobId`) VALUES
+(1, 5, 'Wouter', 'Van den Intranet', 'wouter@intranet.enerjoy.be', '0476001122', 2),
+(2, 1, 'Axel', 'Van den Api', 'axel@api.enerjoy.be', '0476334455', 1);
 
 -- --------------------------------------------------------
 
@@ -101,6 +148,14 @@ CREATE TABLE `invoices` (
   `date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `invoices`
+--
+
+INSERT INTO `invoices` (`id`, `customerId`, `price`, `date`) VALUES
+(1, 1, 147.23, '2020-02-26 19:32:16'),
+(2, 2, 420.69, '2020-02-24 08:11:14');
+
 -- --------------------------------------------------------
 
 --
@@ -111,6 +166,16 @@ CREATE TABLE `jobs` (
   `id` bigint(8) UNSIGNED NOT NULL,
   `name` varchar(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `jobs`
+--
+
+INSERT INTO `jobs` (`id`, `name`) VALUES
+(1, 'Api'),
+(2, 'Intranet'),
+(3, 'HR'),
+(4, 'Website');
 
 -- --------------------------------------------------------
 
@@ -125,6 +190,14 @@ CREATE TABLE `meters` (
   `isUsed` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `meters`
+--
+
+INSERT INTO `meters` (`id`, `customerId`, `created`, `isUsed`) VALUES
+(1, 2, '2020-02-24 00:00:00', 0),
+(2, 1, '2020-02-20 00:00:00', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -138,6 +211,15 @@ CREATE TABLE `meter_data` (
   `time` time NOT NULL,
   `consumption` int(16) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `meter_data`
+--
+
+INSERT INTO `meter_data` (`id`, `linkedMeterId`, `date`, `time`, `consumption`) VALUES
+(1, 1, '2020-02-25', '19:00:00', 3000),
+(2, 1, '2020-02-26', '19:00:00', 3700),
+(3, 2, '2020-02-25', '06:12:07', 2756);
 
 -- --------------------------------------------------------
 
@@ -154,6 +236,14 @@ CREATE TABLE `suppliers` (
   `phonenumber` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `suppliers`
+--
+
+INSERT INTO `suppliers` (`id`, `companyname`, `vatnumber`, `email`, `addressId`, `phonenumber`) VALUES
+(1, 'GreenEnergy', 'BE1234567890', 'supply@greenenergy.be', 1, '0477001122'),
+(2, 'RUSpower', 'RU0011223344', 'energy@ruspower.ru', 4, '0123456789');
+
 -- --------------------------------------------------------
 
 --
@@ -168,6 +258,14 @@ CREATE TABLE `tarif` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Dumping data for table `tarif`
+--
+
+INSERT INTO `tarif` (`id`, `name`, `description`, `value`) VALUES
+(1, 'Goedkoop plan', 'Niet zo duur', 19.99),
+(2, 'Duur plan', 'Wel zo duur', 49.99);
+
+--
 -- Indexes for dumped tables
 --
 
@@ -176,14 +274,14 @@ CREATE TABLE `tarif` (
 --
 ALTER TABLE `addresses`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `countryAddr` (`countryId`),
   ADD KEY `cityAddr` (`cityId`);
 
 --
 -- Indexes for table `city`
 --
 ALTER TABLE `city`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `cityCountry` (`countryId`);
 
 --
 -- Indexes for table `country`
@@ -255,67 +353,67 @@ ALTER TABLE `tarif`
 -- AUTO_INCREMENT for table `addresses`
 --
 ALTER TABLE `addresses`
-  MODIFY `id` bigint(8) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `city`
 --
 ALTER TABLE `city`
-  MODIFY `id` bigint(8) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `country`
 --
 ALTER TABLE `country`
-  MODIFY `id` bigint(8) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `id` bigint(8) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `employees`
 --
 ALTER TABLE `employees`
-  MODIFY `id` bigint(8) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `invoices`
 --
 ALTER TABLE `invoices`
-  MODIFY `id` bigint(8) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `jobs`
 --
 ALTER TABLE `jobs`
-  MODIFY `id` bigint(8) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `meters`
 --
 ALTER TABLE `meters`
-  MODIFY `id` bigint(8) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `meter_data`
 --
 ALTER TABLE `meter_data`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `suppliers`
 --
 ALTER TABLE `suppliers`
-  MODIFY `id` bigint(8) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tarif`
 --
 ALTER TABLE `tarif`
-  MODIFY `id` bigint(8) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
@@ -325,8 +423,13 @@ ALTER TABLE `tarif`
 -- Constraints for table `addresses`
 --
 ALTER TABLE `addresses`
-  ADD CONSTRAINT `cityAddr` FOREIGN KEY (`cityId`) REFERENCES `city` (`id`),
-  ADD CONSTRAINT `countryAddr` FOREIGN KEY (`countryId`) REFERENCES `country` (`id`);
+  ADD CONSTRAINT `cityAddr` FOREIGN KEY (`cityId`) REFERENCES `city` (`id`);
+
+--
+-- Constraints for table `city`
+--
+ALTER TABLE `city`
+  ADD CONSTRAINT `cityCountry` FOREIGN KEY (`countryId`) REFERENCES `country` (`id`);
 
 --
 -- Constraints for table `customers`
