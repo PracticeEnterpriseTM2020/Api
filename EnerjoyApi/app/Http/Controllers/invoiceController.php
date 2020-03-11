90d2e7868bd43b8ffb84d4ae0790531a3d1eefe3
wocowoco <wouter360@hotmail.com>
2020-03-11 16:59:15 +0100

    Added temp code for adding invoices to DB

--- EnerjoyApi/app/Http/Controllers/invoiceController.php
+++ EnerjoyApi/app/Http/Controllers/invoiceController.php
@@ -32,4 +32,34 @@ class invoiceController extends Controller
         //Return the invoice from the requested ID
         return invoice::where('id',$invoiceId)->get();
     }
+
+    
+     //Store a newly created resource in storage.
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'meter_id' => 'required|max:255',
+            'creation_timestamp' => 'required|max:16|min:16|date'
+        ]);
+
+        //If the data entered isn't valid, throw error and don't add to DB
+        if ($validator->fails()) {
+            return response()->json(['success' => false, 'errors' => $validator->messages()], 422);
+        }
+
+        $invoice = new Invoice();
+
+        $invoice->id = request('id');
+        $invoice->customerId = request('customerId');
+        $invoice->price = request('price');
+        $invoice->date = 
+        $meter->save();
+
+        if (!$meter->save()) {
+            return response()->json(['success' => false, 'errors' => 'Data has not been added to database.'], 422);
+        } else {
+            return response()->json(['success' => true, 'message' => 'Data added to database.'], 200);
+        }
+    }
+
 }
